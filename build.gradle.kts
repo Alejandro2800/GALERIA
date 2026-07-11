ff992a6c604db3b139f13c83e28abc8a90c28017
Alejandro2800 <38256518+Alejandro2800@users.noreply.github.com>
2026-07-11 15:32:02 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,6 @@
+// Archivo build.gradle.kts de nivel raíz del proyecto
 
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
